73f10f7939d2711f59815901ed24d4564dbaa4d6
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-08 16:40:12 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
   end
   
   def parse 
-    email_list = @text.split(/[ ,]/).reject {|email| email.empty? }
+    @text.split(/[ ,]/).reject {|email| email.empty? }.uniq
     email_list.uniq
   end 
 end 
